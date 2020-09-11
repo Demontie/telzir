e49bedecdbaefc963b2affa51989ec86b2e7eca8
Erandir Junior <aefs12junior@gmail.com>
2020-09-11 16:23:12 -0300

    Starting with tests

---
+++ backend/tests/CallCost.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SRC\Test;
+
+use PHPUnit\Framework\TestCase;
+use SRC\Application\Boundery\InputCallData;
+use SRC\Domain\CallCost\CalculateCallCost;
+
+class CallCost extends TestCase
+{
+    private $domain;
+
+    protected function setUp(): void
+    {
+        $this->domain = new CalculateCallCost(new Repository());
+    }
+
+    public function testCalculatePlan30()
+    {
+        $input = new InputCallData(1, 2, 1, 20);
+        $current = $this->domain->calculate($input);
+        $expected = [
+            'from' => '011',
+            'to'   => '016',
+            'plan' => 'FaleMais 30',
+            'withPlan' => '0',
+            'withoutPlan' => '38'
+        ];
+        $this->assertEquals($expected, $current);
+    }
+
+    public function testCalculatePlan60()
+    {
+        $input = new InputCallData(1, 3, 2, 80);
+        $current = $this->domain->calculate($input);
+        $expected = [
+            'from' => '011',
+            'to'   => '017',
+            'plan' => 'FaleMais 60',
+            'withPlan' => 37.40,
+            'withoutPlan' => 136
+        ];
+        $this->assertEquals($expected, $current);
+    }
+
+    public function testCalculatePlan120()
+    {
+        $input = new InputCallData(4, 1, 3, 200);
+        $current = $this->domain->calculate($input);
+        $expected = [
+            'from' => '018',
+            'to'   => '011',
+            'plan' => 'FaleMais 120',
+            'withPlan' => 167.20,
+            'withoutPlan' => 380
+        ];
+        $this->assertEquals($expected, $current);
+    }
+
+    public function testCalculateEmptyCall()
+    {
+        $input = new InputCallData(4, 3, 1, 100);
+        $current = $this->domain->calculate($input);
+        $expected = [
+            'from' => '018',
+            'to'   => '017',
+            'plan' => 'FaleMais 30',
+            'withPlan' => '-',
+            'withoutPlan' => '-'
+        ];
+        $this->assertEquals($expected, $current);
+    }
+}
