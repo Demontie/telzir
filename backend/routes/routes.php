c0f80d87d56cad124ab22f7fbbfe3f99ebe615aa
Erandir Junior <aefs12junior@gmail.com>
2020-09-11 16:23:31 -0300

    Added new routes

--- backend/routes/routes.php
+++ backend/routes/routes.php
@@ -10,4 +10,10 @@ $route->options('/{anything}', function () {
     return '';
 });
 
+$route->post('/', 'SRC\Infrastructure\InputHandler\CallCostInput@calculate');
+
+$route->get('/plans', 'SRC\Infrastructure\InputHandler\Plan@findAll');
+
+$route->get('/areas', 'SRC\Infrastructure\InputHandler\AreaCode@findAll');
+
 $route->on();
